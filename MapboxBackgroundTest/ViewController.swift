56b1986763cce3dfeec8380122e7282a7611a0ae
Tobias Ottenweller <tobias.ottenweller@intive-kupferwerk.com>
2019-08-16 11:18:50 +0200

    Sample code added.

--- MapboxBackgroundTest/ViewController.swift
+++ MapboxBackgroundTest/ViewController.swift
@@ -6,15 +6,14 @@
 //  Copyright © 2019 Tobias Ottenweller. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let locationManager = CLLocationManager()
+
+    @IBAction func requestAuthorization(_ sender: Any) {
+        locationManager.requestAlwaysAuthorization()
     }
-
-
 }
-
